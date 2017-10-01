611d1e3cec389fe12aafbfbc5551f8a9ce01ef43
Matt Rulona <mattrulona98@gmail.com>
2017-10-01 18:12:23 +0800

    added contents for Basic Information

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ function openTab(evt, tabName) {
         tablinks[i].className = tablinks[i].className.replace(" active", " ");
     }
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
